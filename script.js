ffda338cdf282adfa5bb02f168499793eb30c140
Lafortune Pierre <lafortune.pierre47@gmail.com>
2017-03-21 06:57:12 +0100

    Introducing graphics - final is trembling monsters

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+// Inits
+window.onload = function init() {
+    var game = new GF();
+    game.start();
+};
+
+
+// GAME FRAMEWORK STARTS HERE
 var GF = function(){
+    // Vars relative to the canvas
+    var canvas, ctx, w, h; 
+
     // vars for counting frames/s, used by the measureFPS function
     var frameCount = 0;
     var lastTime;
     var fpsContainer;
-    var randomNumberContainer;
     var fps; 
     
     var measureFPS = function(newTime){
@@ -29,27 +39,74 @@ var GF = function(){
 	frameCount++;
     };
     
+    // clears the canvas content
+    function clearCanvas() {
+	ctx.clearRect(0, 0, w, h);
+    }
+    
+    // Functions for drawing the monster and maybe other objects
+    function drawMyMonster(x, y) {
+	// draw a big monster !
+	// head
+	
+	// save the context
+	ctx.save();
+	
+	// translate the coordinate system, draw relative to it
+	ctx.translate(x, y);
+	
+	// (0, 0) is the top left corner of the monster.
+	ctx.strokeRect(0, 0, 100, 100);
+	
+	// eyes
+	ctx.fillRect(20, 20, 10, 10);
+	ctx.fillRect(65, 20, 10, 10);
+	
+	// nose
+	ctx.strokeRect(45, 40, 10, 40);
+	
+	// mouth
+	ctx.strokeRect(35, 84, 30, 10);
+	
+	// teeth
+	ctx.fillRect(38, 84, 10, 10);
+	ctx.fillRect(52, 84, 10, 10);
+	
+	// restore the context
+	ctx.restore(); 
+    }
+    
     var mainLoop = function(time){
-	//Here goes the main Actions
-	randomNumberContainer.innerHTML = Math.random();
-
-	// compute FPS, called each frame, uses the high resolution
-	// time parameter given by the browser that implements the
-	// requestAnimationFrame API
-	// main function, called each frame
+        //main function, called each frame 
         measureFPS(time);
+	
+        // Clear the canvas
+        clearCanvas();
         
+        // draw the monster
+        drawMyMonster(10+Math.random()*10, 10+Math.random()*10);
+	
         // call the animation loop every 1/60th of second
         requestAnimationFrame(mainLoop);
     };
 
     var start = function(){
-        fpsContainer = document.getElementById('framePerSeconds');
-	randomNumberContainer = document.getElementById('randomNumber');
-	console.log("fpsContainer = " + fpsContainer);
-	console.log("randomNumberContainer = " + randomNumberContainer);
+        // adds a div for displaying the fps value
+        fpsContainer = document.createElement('div');
+        document.body.appendChild(fpsContainer);
+	
+        // Canvas, context etc.
+        canvas = document.querySelector("#myCanvas");
+	
+        // often useful
+        w = canvas.width; 
+        h = canvas.height;  
+	
+        // important, we will draw with this object
+        ctx = canvas.getContext('2d');
 
-	requestAnimationFrame(mainLoop);
+        // start the animation
+        requestAnimationFrame(mainLoop);
     };
 
     //our GameFramework returns a public API visible from outside its scope
@@ -57,14 +114,3 @@ var GF = function(){
         start: start
     };
 };
-
-var game = new GF();
-game.start();
-
-
-
-
-
-
-
-
