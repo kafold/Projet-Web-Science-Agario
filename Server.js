6a7e988528d1b5037ae5132471b3a4e05837d02b
Lafortune Pierre <lafortune.pierre47@gmail.com>
2017-06-07 06:20:37 +0200

    On part a partir du tp - ajout de modifs

--- Server.js
+++ Server.js
@@ -1,89 +1,91 @@
+function Server (portNumber) {
+    // We need to use the express framework: have a real web servler that knows how to send mime types etc.
+    this.express = require('express');
+    this.app = null;
+    this.http = null;
+    this.server = null;
+    this.io = null;
+    // usernames which are currently connected to the chat
+    this.usernames = {};
+    this.listOfPlayers = {};
+    var init = function () {
+        // Init globals variables for each module required
+        this.app = this.express();
+        this.http = require('http');
+        this.server = http.createServer(app);
+        this.io = require('socket.io').listen(this.server);
 
-// We need to use the express framework: have a real web servler that knows how to send mime types etc.
-var express=require('express');
+        // launch the http server on given port
+        this.server.listen(portNumber);
 
-// Init globals variables for each module required
-var app = express()
-    , http = require('http')
-    , server = http.createServer(app)
-    , io = require('socket.io').listen(server);
+        // Indicate where static files are located. Without this, no external js file, no css...
+        this.app.use(this.express.static(__dirname + '/'));
 
-// launch the http server on given port
-server.listen(8082);
 
-// Indicate where static files are located. Without this, no external js file, no css...  
-app.use(express.static(__dirname + '/'));
+        // routing
+        this.app.get('/', function (req, res) {
+            res.sendfile(__dirname + '/simpleChat.html');
+        });
 
-// routing
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
+        this.io.sockets.on('connection', function (socket) {
 
-// usernames which are currently connected to the chat
-var usernames = {};
-var listOfPlayers = {};
+            // when the client emits 'sendchat', this listens and executes
+            socket.on('sendchat', function (data) {
+                // we tell the client to execute 'updatechat' with 2 parameters
+                this.io.sockets.emit('updatechat', socket.username, data);
+            });
 
-// array of balls to animate
-var ballsArray = [];
+            // when the client emits 'sendchat', this listens and executes
+            socket.on('sendpos', function (newPos) {
+                // we tell the client to execute 'updatepos' with 2 parameters
+                //console.log("recu sendPos");
+                socket.broadcast.emit('updatepos', socket.username, newPos);
+            });
 
-//array of players
-var playersArray = [];
+            // when the client emits 'adduser', this listens and executes
+            socket.on('adduser', function (username) {
+                // we store the username in the socket session for this client
+                // the 'socket' variable is unique for each client connected,
+                // so we can use it as a sort of HTTP session
+                socket.username = username;
+                // add the client's username to the global list
+                // similar to usernames.michel = 'michel', usernames.toto = 'toto'
+                this.usernames[username] = username;
+                // echo to the current client that he is connecter
+                socket.emit('updatechat', 'SERVER', 'you have connected');
+                // echo to all client except current, that a new person has connected
+                socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+                // tell all clients to update the list of users on the GUI
+                io.sockets.emit('updateusers', this.usernames);
 
-io.sockets.on('connection', function (socket) {
+                // Create a new player and store his position too... for that
+                // we have an object that is a "list of players" in that form
+                // listOfPlayer = {'michel':{'x':0, 'y':0, 'v':0},
+                // 							john:{'x':10, 'y':10, 'v':0}}
+                // for this example we have x, y and v for speed... ?
+                var player = {'x': 0, 'y': 0, 'v': 0}
+                this.listOfPlayers[username] = player;
+                io.sockets.emit('updatePlayers', this.listOfPlayers);
+            });
 
-    // when the client emits 'sendchat', this listens and executes
-    socket.on('sendchat', function (data) {
-        // we tell the client to execute 'updatechat' with 2 parameters
-        io.sockets.emit('updatechat', socket.username, data);
-    });
+            // when the user disconnects.. perform this
+            socket.on('disconnect', function () {
+                // remove the username from global usernames list
+                delete this.usernames[socket.username];
+                // update list of users in chat, client-side
+                io.sockets.emit('updateusers', this.usernames);
 
-    // when the client emits 'sendchat', this listens and executes
-    socket.on('sendpos', function (newPos) {
-        // we tell the client to execute 'updatepos' with 2 parameters
-        //console.log("recu sendPos");
-        socket.broadcast.emit('updatepos', socket.username, newPos);
-    });
+                // Remove the player too
+                delete this.listOfPlayers[socket.username];
+                io.sockets.emit('updatePlayers', this.listOfPlayers);
 
-    // when the client emits 'adduser', this listens and executes
-    socket.on('adduser', function(username, arrays){
-        // we store the username in the socket session for this client
-        // the 'socket' variable is unique for each client connected,
-        // so we can use it as a sort of HTTP session
-        socket.username = username;
-        ballsArray = arrays.ballsArray;
-        playersArray = arrays.playersArray;
-        // add the client's username to the global list
-        // similar to usernames.michel = 'michel', usernames.toto = 'toto'
-        usernames[username] = username;
-        // echo to the current client that he is connecter
-        socket.emit('updatechat', 'SERVER', 'you have connected');
-        // echo to all client except current, that a new person has connected
-        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-        // tell all clients to update the list of users on the GUI
-        io.sockets.emit('updateusers', usernames, arrays);
+                // echo globally that this client has left
+                socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+            });
+        });
+    };
 
-        // Create a new player and store his position too... for that
-        // we have an object that is a "list of players" in that form
-        // listOfPlayer = {'michel':{'x':0, 'y':0, 'v':0},
-        // 							john:{'x':10, 'y':10, 'v':0}}
-        // for this example we have x, y and v for speed... ?
-        var player = {'x':0, 'y':0, 'v':0}
-        listOfPlayers[username] = player;
-        io.sockets.emit('updatePlayers',listOfPlayers);
-    });
-
-    // when the user disconnects.. perform this
-    socket.on('disconnect', function(){
-        // remove the username from global usernames list
-        delete usernames[socket.username];
-        // update list of users in chat, client-side
-        io.sockets.emit('updateusers', usernames);
-
-        // Remove the player too
-        delete listOfPlayers[socket.username];
-        io.sockets.emit('updatePlayers',listOfPlayers);
-
-        // echo globally that this client has left
-        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-    });
-});
+    return{
+        init : init
+    }
+}
